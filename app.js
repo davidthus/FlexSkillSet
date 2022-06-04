ac911e30fd0b1d6626552a4536d26d2696e94050
davidthus <dvdnicorici05@gmail.com>
2022-06-04 12:32:52 +0100

    glitched

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const wrapper = document.querySelector(".slider-wrapper");
 const sliderItems = document.querySelectorAll(".item");
+const leftArrows = document.querySelectorAll(".arrow_left");
+const rightArrows = document.querySelectorAll(".arrow_right");
+
+leftArrows.forEach((arrow, index) => {
+  arrow.addEventListener("click", () => {
+    wrapper.style.transform = "translateX(100vw)";
+  });
+});
+
+rightArrows.forEach((arrow, index) => {
+  arrow.addEventListener("click", () => {
+    wrapper.style.transform = "translateX(-100vw)";
+  });
+});
